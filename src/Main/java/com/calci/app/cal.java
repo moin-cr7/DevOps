e02e3c2629f286ca6bd3cc90d655e0729668dfe1
Moin <moingorikhancr7@gmail.com>
2023-02-21 13:18:31 +0530

    CalculatorWithTestcases

---
+++ src/Main/java/com/calci/app/cal.java
@@ -0,0 +1,36 @@
+package Main.java.com.calci.app;
+public class cal{
+
+	public static int add(int a, int b){
+		
+		return a+b;
+
+	}
+
+	 public static int sub(int a, int b){
+
+                return a-b;
+
+        }
+	
+	 public static int mul(int a, int b){
+
+                return a*b;
+
+        }
+	
+	 public  static int div(int a, int b){
+
+                return a/b;
+
+        }
+	
+	 public static int mod(int a, int b){
+
+                return a%b;
+
+        }
+	
+
+
+}
---
+++ src/Test/java/com/calci/app/Testcal.java
@@ -0,0 +1,64 @@
+package test.java.com.calci.app;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import main.java.com.calci.app.cal;
+ public class Testcal{
+
+	@Test
+	void testAdd() {
+		
+		assertEquals(40, cal.add(10, 30));
+		assertEquals(60, cal.add(30, 30));
+		assertEquals(400, cal.add(100, 300));
+		assertEquals(42, cal.add(21, 21));
+		assertEquals(40, cal.add(1, 39));
+		
+		}
+		
+	
+	@Test
+	void testSub() {
+		
+		assertEquals(2, cal.sub(6, 4));
+		assertEquals(60, cal.sub(100, 40));
+		assertEquals(400, cal.sub(1000, 600));
+		assertEquals(42, cal.sub(63, 21));
+		assertEquals(40, cal.sub(79, 39));
+		
+		}
+	
+	@Test
+	void testDiv() {
+		
+		assertEquals(4, cal.div(12, 3));
+		assertEquals(0, cal.div(0, 30));
+		assertEquals(3, cal.div(9, 3));
+		assertEquals(8, cal.div(64,8));
+		assertEquals(0, cal.div(0, 6));
+		
+		}
+	
+	
+	@Test
+	void testMul() {
+		
+		assertEquals(-2000, cal.mul(-50, 40));
+		assertEquals(60, cal.mul(3, 20));
+		assertEquals(400, cal.mul(20, 20));
+		assertEquals(42, cal.mul(21, 2));
+		assertEquals(40, cal.mul(1, 40));
+		
+		}
+	
+	@Test
+	void testMod() {
+		
+		assertEquals(1, cal.mod(1, 2));
+		assertEquals(0, cal.mod(200, 2));
+		assertEquals(2, cal.mod(8, 3));
+		assertEquals(4, cal.mod(19,5));
+		assertEquals(0, cal.mod(32, 8));
+		
+		}	
+
+}
